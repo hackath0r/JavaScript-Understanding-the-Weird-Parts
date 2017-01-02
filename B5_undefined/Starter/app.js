cda61227d67ae97cfb69f1ca4c9282545f34757b
Prashant <prashant.bhrgv@gmail.com>
2017-01-03 02:59:59 +0530

    javascript undefined

---
+++ B5_undefined/Starter/app.js
@@ -0,0 +1,11 @@
+var a;
+console.log(a);
+
+// undefined is a special value assigned to javascript varibles
+// in the execution context
+if (a === undefined) {
+    console.log('a is undefined!');
+}
+else {
+    console.log('a is defined!');
+}
